4eef0d5e6eecc15a94415c0e8b01fdfdebd65427
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-03 17:19:07 -0300

    [Ilton Garcia]: Commit com a definição da chamada dos laods dos componentes no document ready.

--- assets/js/ton_interacoes.js
+++ assets/js/ton_interacoes.js
@@ -1,5 +1,11 @@
 //Definição do que deve ser feito quando o documento estiver pronto
 $(document).ready(function(){
-	//Carregamento da primeira aba
-	
+	//Carregamento da formação
+	load_formacao();
+	//Carregamento da experiência
+	load_experiencias();
+	//Carregamento dos projetos
+	load_projetos();
+	//Carregamento dos TODOs
+	load_todos();
 });
