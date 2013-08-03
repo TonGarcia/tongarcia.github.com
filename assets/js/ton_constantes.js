061553f38bf2df15fe9e654e5ea58c4b67cbd47b
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-03 17:17:35 -0300

    [Ilton Garcia]: Redefinição das constantes (localização e nomes) por erro no ajax.

--- assets/js/ton_constantes.js
+++ assets/js/ton_constantes.js
@@ -1,13 +1,12 @@
+//Definição do diretório e dos arquivos de resposta
+var str_ajax_resps_dir = "ajax_resps/";
+var str_ajax_resps_formacao_dir = str_ajax_resps_dir+"formacao.html";
+var str_ajax_resps_experiencias_dir = str_ajax_resps_dir+"experiencias.html";
+var str_ajax_resps_projetos_dir = str_ajax_resps_dir+"projetos.html";
+var str_ajax_resps_todos_dir = str_ajax_resps_dir+"todos.html";
+
 //Seletores das divs de conteúdo das abas
 var str_sel_tabFormacao = "#tabFormacao";
-var str_sel_tabFormacao = "#tabExperiencias";
-var str_sel_tabFormacao = "#tabProjetos";
-var str_sel_tabFormacao = "#tabTODOs";
-
-var str_ajax_resps_dir = "ajax_resps/";
-var str_ajax_resps_formacao_dir = "formacao.html";
-var str_ajax_resps_experiencias_dir = "experiencias.html";
-var str_ajax_resps_projetos_dir = "projetos.html";
-var str_ajax_resps_todos_dir = "todos.html";
-
-
+var str_sel_tabExperiencias = "#tabExperiencias";
+var str_sel_tabProjetos = "#tabProjetos";
+var str_sel_tabTODOs = "#tabTODOs";
