6a145783ad52442f9e17dc9e1ad172fb1b6df840
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-03 17:18:35 -0300

    [Ilton Garcia]: Adição das funções responsáveis por executarem as requisições ajax de cada uma das abas.

--- assets/js/ton_funcoes.js
+++ assets/js/ton_funcoes.js
@@ -5,6 +5,18 @@ function ajax_get(url, seletor_destino){
 	});
 }
 
-function load_formacao () {
-	
+function load_formacao() {
+	ajax_get(str_ajax_resps_formacao_dir, str_sel_tabFormacao);
+}
+
+function load_experiencias() {
+	ajax_get(str_ajax_resps_experiencias_dir, str_sel_tabExperiencias);
+}
+
+function load_projetos() {
+	ajax_get(str_ajax_resps_projetos_dir, str_sel_tabProjetos);
+}
+
+function load_todos() {
+	ajax_get(str_ajax_resps_todos_dir, str_sel_tabTODOs);
 }
