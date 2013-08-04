ce28e50f0d66d2373e59584e80d009cd25a894ef
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-04 00:59:08 -0300

    [Ilton Garcia]: Refatoração do algoritmo de manipulação dos clicks sobre os botões da navegação entre formações.

--- ajax_resps/formacao.js
+++ ajax_resps/formacao.js
@@ -1,7 +1,16 @@
 //Seletor botões btn-group (UPgrade Bootstrap)
 var btns_group = ".btn-group .btn";
+var content_navegaveis = "#navegaveis > div";
 
 $(btns_group).click(function(){
+        var btn_clicado = $(this);
+        if(btn_clicado.hasClass("active")) return;
 	$(btns_group).removeClass("active");
 	$(this).addClass("active");
+        
+        $(content_navegaveis).addClass("hide");
+        var id_btn = btn_clicado.attr("id");
+        var id_content = id_btn.substring(id_btn.indexOf("_")+1, id_btn.length);
+        
+        $("#"+id_content).removeClass("hide");
 });
