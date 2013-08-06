dcb6b16fc24dc0572c400151f55784b76d3b3db1
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-06 20:42:37 -0300

    [Ilton Garcia]: Refatoração do javascript para ao inves de mover a classe "hide", começar a usar o fade (uso em função do "display").

--- ajax_resps/formacao.js
+++ ajax_resps/formacao.js
@@ -3,14 +3,17 @@ var btns_group = ".btn-group .btn";
 var content_navegaveis = "#navegaveis > div";
 
 $(btns_group).click(function(){
-        var btn_clicado = $(this);
-        if(btn_clicado.hasClass("active")) return;
+    var btn_clicado = $(this);
+    if(btn_clicado.hasClass("active")) return;
+	
 	$(btns_group).removeClass("active");
 	$(this).addClass("active");
         
-        $(content_navegaveis).addClass("hide");
-        var id_btn = btn_clicado.attr("id");
-        var id_content = id_btn.substring(id_btn.indexOf("_")+1, id_btn.length);
-        
-        $("#"+id_content).removeClass("hide");
+    //$(content_navegaveis).addClass("hide");
+    $(content_navegaveis).css("display", "none");
+    var id_btn = btn_clicado.attr("id");
+    var id_content = id_btn.substring(id_btn.indexOf("_")+1, id_btn.length);
+    
+    var content_ref = $("#"+id_content);
+    content_ref.fadeIn('slow', function() {	/* Animation complete */	});
 });
