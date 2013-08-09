edaa5839160088b768e07ba4850ae0a6274d4c11
Ilton Garcia <ilton_unb@hotmail.com>
2013-08-09 01:58:26 -0300

    [Ilton Garcia]: Fim da refatoração do esquema da estrutura de arquivos/diretórios do projeto.

--- ajax_resps/nav_list/nav_list.js
+++ ajax_resps/nav_list/nav_list.js
@@ -1,9 +1,26 @@
 //Constantes
 var str_sel_navList =  ".nav-tabs > li";
-
+var str_sel_scroller = '[data-spy="scroll"]';
 
 $(str_sel_navList).click(function(){
 	$(str_sel_navList).removeClass("active");
 	$(this).addClass("active");
 });
 
+
+/*
+$(".nav-tabs").scrollspy();
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+});
+
+$(str_sel_navList).removeClass("active");
+$("#nav_list > :first-child").addClass("active");
+*/
+
+/*
+$(str_sel_scroller).scroll(function () {
+  console.log("Aconteceu o Scroll!!");
+
+});
+*/
--- ajax_resps/textos_corridos/construtor_texto_corrido.js
+++ ajax_resps/textos_corridos/construtor_texto_corrido.js
@@ -1,6 +1,53 @@
-/*
-$("#texto_corrido_ajax").ready(function(){
-	alert("Testando JS construtor");
-});
-*/
+//Constantes dos diretórios
+var str_ajax_resps_containers = str_ajax_resps_dir+"textos_corridos/containers/";
 
+var str_ajax_resps_cursos = str_ajax_resps_containers+"curso_extensao.html";
+var str_ajax_resps_exps = str_ajax_resps_containers+"experiencias_profissionais.html";
+var str_ajax_resps_graduacao = str_ajax_resps_containers+"graduacao.html";
+var str_ajax_resps_idiomas = str_ajax_resps_containers+"idiomas.html";
+var str_ajax_resps_projs_open = str_ajax_resps_containers+"meus_projs_opensource.html";
+var str_ajax_resps_contri_open = str_ajax_resps_containers+"minhas_contribuicoes_opensource.html";
+
+
+//Constantes dos elementos DOM
+var str_sel_graduacao = "#graduacao";
+var str_sel_cursos_ext = "#cursos_extensao";
+var str_sel_exps = "#experiencias";
+var str_sel_projs_open = "#meus_opensource";
+var str_sel_contribuicoes_open = "#colaboracoes_opensource";
+var str_sel_idiomas = "#idiomas";
+
+timer = 0;
+
+function load_graduacao() {
+	ajax_get(str_ajax_resps_graduacao, str_sel_graduacao);
+}
+
+function load_cursos_extensao() {
+	ajax_get(str_ajax_resps_cursos, str_sel_cursos_ext);
+}
+
+function load_experiencias() {
+	ajax_get(str_ajax_resps_exps, str_sel_exps);
+}
+
+function load_projs_open_source() {
+	ajax_get(str_ajax_resps_projs_open, str_sel_projs_open);
+}
+
+function load_contribuicoes_open_source() {
+	ajax_get(str_ajax_resps_contri_open, str_sel_contribuicoes_open);
+}
+
+function load_idiomas() {
+	ajax_get(str_ajax_resps_idiomas, str_sel_idiomas);
+}
+
+$("#texto_corrido_ajax").ready(function(){
+	load_graduacao();
+	load_cursos_extensao();
+	load_experiencias();
+	load_projs_open_source();
+	load_contribuicoes_open_source();
+	load_idiomas();
+});
